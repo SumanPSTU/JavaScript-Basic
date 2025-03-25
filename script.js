523ae0eae27cf70ec5892cadce8f43521923cac5
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-03-25 11:44:17 +0600

    upload file

---
+++ script.js
@@ -0,0 +1,14 @@
+let x = "string";
+let y = "star";
+if(x>y){
+    console.log(x.toUpperCase());
+}else{
+    console.log(y)
+}
+
+let text = x+" "+y;
+console.log(text);
+
+let v = "what a nice day";
+v+=" also funny";
+console.log(v);
