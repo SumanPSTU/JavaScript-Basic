1b2d1d1e9bc83ae52c6b3ce7c56502155f336958
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-03-25 16:42:45 +0600

    update fiel

--- script.js
+++ script.js
@@ -38,3 +38,41 @@ if(x!=y){
 // }else{
 //     console.log("logic is false");
 // }
+
+// for(let i = 0;i<=10;i++){
+//     console.log("The number is "+(i-1));
+// }
+
+{
+    let x = 5;
+    let y = 2;
+    let z = x - y;
+    console.log(z);
+}
+
+
+let m = 5;
+console.log(++m);
+console.log(m++);
+console.log(m);
+
+// javascript string 
+let ans1 = "It's alright";
+let ans2 = "He is called 'Johny'";
+let ans3 = 'he is called "Jhonny"';
+console.log(ans1);
+console.log(ans2);
+console.log(ans3);
+
+//Javascript number
+let x1 = 34.00;
+let x2 = 34;
+console.log(x1);
+console.log(x2);
+
+//javascript exponenttial notation 
+
+let y1 = 123e5;
+let y2 = 123e-5;
+console.log(y1);
+console.log(y2);
