7ccdb459dd70242681c46b18cd4003faa7c4fea9
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-03-25 16:20:16 +0600

    u

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ console.log(text);
 let v = "what a nice day";
 v+=" also funny";
 console.log(v);
+
+let a = 5 + 5;
+let b = "5" + 5;
+let c = "hello" + 5;
+let d = "5" + "5";
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let xx = true;
+let yy = false;
+
+if(x!=y){
+    console.log("x is not equal y");
+}else{
+    console.log("X is equal y");
+}
+
+// if(xx && yy){
+//     console.log("logic is true");
+// }else{
+//     console.log("logic is false");
+// }
